d3bfc52bf2bc7fa92a23b39e46f169f531969ade
abdoaswe <abdoahmed4434@gmail.com>
2023-02-10 09:27:15 +0200

    lab1

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponentComponent,
+    AboutMeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
